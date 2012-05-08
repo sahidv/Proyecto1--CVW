4a07a9de74bd7a359304b103cbead05a19f1f386
Sahid Villela <sdz1992@gmail.com>
2012-05-07 23:11:13 -0600

    Se redefinieron todas las funciones que heredan y son abstratactas, asi no da error al compilar

--- VampireWarGame/src/VampireWarGame/Muerte.java
+++ VampireWarGame/src/VampireWarGame/Muerte.java
@@ -10,21 +10,34 @@ package VampireWarGame;
  * 
  */
 public class Muerte extends Fichas{
-    protected int lifePoints;
-    protected int damagePoints;
-    protected int cantMovientos;
+    public static final int codFicha = 3;
     
     public Muerte (String nombre, String col){
         super(nombre,col);
     }
     
     @Override
-    protected void setCantMovimientos(int cant){
+    public void setCantMovimientos(int cant){
         cantMovientos = cant;
     }
     
     @Override
-    protected void setCantLP(int lp){
+    public void setCantLP(int lp){
         lifePoints = lp;
+    }
+    
+    @Override
+    public int setDamagePoints(int codFichaContraria){
+        switch (codFichaContraria){
+            case 1:
+                return -3;
+            case 2:
+                return -2;
+            case 3:
+                return -1;
+            case 4:
+                return -2;
+            default: return 0;     
+        } 
     }
 }
