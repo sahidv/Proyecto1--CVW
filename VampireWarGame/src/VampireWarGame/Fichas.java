b2a50cf6f99b9ecaa6d06b336b8290a2de9a4ed3
Henry sevilla <Henry_sevilla.xgs@unitec.edu>
2012-05-09 15:18:56 -0600

    Agrgarda la funcion de cambio de caracter a num (A = 0)

--- VampireWarGame/src/VampireWarGame/Fichas.java
+++ VampireWarGame/src/VampireWarGame/Fichas.java
@@ -22,13 +22,25 @@ public abstract class Fichas {
     
     public abstract void setCantMovimientos(int cant);
     
-    public abstract void setCantLP(int lp);
+    public abstract boolean validarMovieminto(String posInicial, String posDestino);
     
     public abstract int setDamagePoints(int codigoFicha);
     
     public abstract void setMovimientos(String x, String y);
     
     public abstract void Movientos();
+    
+    public int y(char y){
+        switch(y){   
+    case 'A': return 0;
+    case 'B': return 1;
+    case 'C': return 2;
+    case 'D': return 3;
+    case 'E': return 4;
+    case 'F': return 5;
+    default: return -1;
+        }
+    }
 
 //    public abstract void setPOSICION_X(int POSICION_X);
 
