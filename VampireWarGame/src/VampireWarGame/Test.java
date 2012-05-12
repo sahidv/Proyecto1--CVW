4e62aeae3237e68856cb246eb06bb58490da0179
Sahid Villela <sdz1992@gmail.com>
2012-05-12 14:56:54 -0600

    Codigo de prueba - FAIL

--- VampireWarGame/src/VampireWarGame/Test.java
+++ VampireWarGame/src/VampireWarGame/Test.java
@@ -14,10 +14,16 @@ public class Test {
     
     public static void main(String args[]){
    
+        Fichas HL = new HombreLobo("HB", "Blaco");
+        
     Tablero TB=new Tablero();
     TB.Iniciar();
     TB.Imprimir();
-    TB.Movimiento("A1", "C4");
+    //TB.Movimiento("A1", "C4");
+    //TB.Imprimir();
+    
+    HL.validarMovieminto("A1", "A3");
     TB.Imprimir();
+    
     }
 }
